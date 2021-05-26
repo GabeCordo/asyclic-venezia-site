3cf8d0cb18d9efd98e1326b22279e47f67d7dcb3
GabeCordo <gabe.cordovado@yahoo.ca>
2021-05-26 11:50:30 -0400

    changed port to 8080

--- app.py
+++ app.py
@@ -29,12 +29,12 @@ def index():
 	return render_template('index.html', title='Grackle', sub_title='Official Site', navbar=True, footer=True, under_work = CONSTRUCTION_MODE)
 
 #route a new port for connection to the support page where visitors can either look for
-#the repository of pre-asnwered issues and the ability to open chat logs
+#the repository of pre-answered issues and the ability to open chat logs
 @app.route('/troubleshooting', methods=['post', 'get'])
 def troubleshooting():
 	return render_template('troubleshooting.html', title='Grackle', sub_title='Find a Solution', navbar=True, footer=True, under_work = CONSTRUCTION_MODE)
 
-#route a new port for connection to the signup page for creating new accounts on the 
+#route a new port for connection to the sign up page for creating new accounts on the 
 #manakin platform for encrypted message communication
 @app.route('/login', methods=['post', 'get'])
 def signup():
@@ -47,6 +47,6 @@ def handle(error):
 	return render_template('unkownaddr.html', title='Grackle', sub_title="Something's not Right", navbar=False, footer=False, under_work = CONSTRUCTION_MODE)
 	
 #we only want to run this server from this file, only during the build-phase should we have
-#debugging turned on which will automaticly update the page on commit
+#debugging turned on which will automatically update the page on commit
 if __name__ == "__main__":
-	app.run(debug=DEBUG_MODE, host='0.0.0.0')
+	app.run(debug=DEBUG_MODE, host='0.0.0.0', port='8080')
